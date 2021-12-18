3b57252ed843c4d41bb6ac75c6cf7c107cd81368
shridrgk <shridhara055@gmail.com>
2021-12-18 16:58:33 +0530

    ustimezone

--- testspec.ts
+++ testspec.ts
@@ -26,6 +26,9 @@ describe("testing program", async () => {
     await add(3, 5);
     await add(5, 4);
     await add(5, 5);
+    await add(5, 10);
+    await add(5, 25);
+
     await calc.getResult.count().then(function (res1) {
       console.log(res1);
     });
